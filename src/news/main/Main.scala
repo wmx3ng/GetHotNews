64456ed959028df5d2a6251f94aa4f33d4bdb721
Wang Mingxing <wmx@deepdata.com.cn>
2016-01-28 17:59:50 +0800

    删除一个分号

---
+++ src/news/main/Main.scala
@@ -0,0 +1,25 @@
+package news.main
+
+import java.io.PrintWriter
+
+import news.baidu.hotpoint.GetBaiduHotPoints
+import news.takungpao.titles.GetTKPNews
+
+/**
+  * Created by wang on 1/28/16.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    val baiduHPs = GetBaiduHotPoints.getHotPoints()
+    val tkpTitles = GetTKPNews.getTKPNews()
+    val res = (baiduHPs ::: tkpTitles).sortWith((kw1, kw2) => kw1.compareToIgnoreCase(kw2) < 0).distinct
+    //    res.foreach(println)
+    //写到文件中,定时程序处理文件;
+    val writer = new PrintWriter("newData", "UTF-8")
+    try {
+      res.foreach { x => writer.println(x) }
+    } finally {
+      writer.close()
+    }
+  }
+}
